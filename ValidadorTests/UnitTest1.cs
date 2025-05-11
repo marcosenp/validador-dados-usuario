803f6d1fe9425a42ff8343fce2708fc1f85f3573
marcosenp <marcospne2103@gmail.com>
2025-05-11 20:33:50 -0300

    feat: adicionar solução, projeto de testes e configuração inicial

---
+++ ValidadorTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+public class UsuarioTests
+{
+    [Theory]
+    [InlineData("12345678901", true)]
+    [InlineData("1234567890", false)]
+    [InlineData("abcdefghijk", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    public void TestarValidacaoCpf(string cpf, bool esperado)
+    {
+        bool resultado = Program.ValidarCpf(cpf);
+
+        Assert.Equal(esperado, resultado);
+    }
+}
